cb6328e9ed384aa6a242c660fe2c6a6c6fb3f65f
danimartin82 <elhombresinremedio@gmail.com>
2021-12-04 20:45:53 +0100

    Added new images and animations

--- main.c
+++ main.c
@@ -32,7 +32,9 @@ typedef struct
     PhysicsBody body;
     int radius;
     bool enable;
-    Color color;
+    int explosion;
+    int color;
+    Vector2 final_position;
 }ball_s;
 
 typedef struct 
@@ -72,7 +74,7 @@ const int screenWidth = 800;
 const int screenHeight = 450;
 
 int getdistance(Vector2 p1, Vector2 p2);
-void new_ball(int radius, Vector2 speed, Vector2 position, Color color);
+void new_ball(int radius, Vector2 speed, Vector2 position, int color);
 void new_random_ball(void);
 
 int main(void)
@@ -84,11 +86,16 @@ int main(void)
     SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(screenWidth, screenHeight, "my first videogame");
 
-    Sound sound1 = LoadSound("audio/Jump.wav");  
+    Sound sound1 = LoadSound("audio/Explosion.wav");  
     Sound music = LoadSound("audio/mixkit-game-level-music-689.wav");
     Sound fire_sound = LoadSound("audio/Fire 1.mp3");
-    Texture2D ball_image = LoadTexture("images/ball1.png");
-    Texture2D mira_image = LoadTexture("images/mira.png");
+    Texture2D ball_image_1 = LoadTexture("images/ball3.png");
+    Texture2D ball_image_2 = LoadTexture("images/ball4.png");
+    Texture2D ball_image_3 = LoadTexture("images/ball5.png");
+    Texture2D ball_images[3]={ball_image_1, ball_image_2, ball_image_3};
+    Texture2D explosion = LoadTexture("images/explosion.png");
+
+    Texture2D mira_image = LoadTexture("images/mira2.png");
     Texture2D landscape = LoadTexture("images/landscape2.png");
     Font ArcadeFont = LoadFont("fonts/ARCADECLASSIC.TTF");  
 
@@ -197,8 +204,10 @@ int main(void)
                    }
 
                    PlaySound(sound1);
-                   balls[i].enable = false;
-                   DestroyPhysicsBody(balls[i].body);
+                   //balls[i].enable = false;
+                   balls[i].final_position = balls[i].body->position;
+                   DestroyPhysicsBody(balls[i].body);              
+                   balls[i].explosion = 16;
                    points +=10;
                    break;
  
@@ -225,19 +234,50 @@ int main(void)
         // Draw balls
         for (int i = 0; i < MAX_NUM_BALLS; i++)
         {
-            if(balls[i].enable == true)
+            if((balls[i].enable == true) && (balls[i].explosion == 0))
             {
-               // DrawCircle(balls[i].body->position.x ,balls[i].body->position.y, balls[i].radius, balls[i].color);
-               // ImageResize(&ball_image, balls[i].radius*2,balls[i].radius*2);
-
                 Vector2 pos = (Vector2){ balls[i].body->position.x - balls[i].radius, balls[i].body->position.y - balls[i].radius};
-                float scale = (float)(2* balls[i].radius) / (float)ball_image.width;
-                DrawTextureEx(ball_image, pos, 0.0, scale, LIGHTGRAY);
+                float scale = (float)(2* balls[i].radius) / (float)ball_image_1.width;
+                DrawTextureEx(ball_images[balls[i].color],pos, 0.0, scale, WHITE);
             }
+        }
+        // Draw explosions
+        for (int i = 0; i < MAX_NUM_BALLS; i++)
+        {
+            if((balls[i].enable == true) && (balls[i].explosion != 0))
+            {
+                Vector2 pos = (Vector2){balls[i].final_position.x - balls[i].radius, balls[i].final_position.y - balls[i].radius};
+                balls[i].explosion -= 1;
+                int sprite = 0;
+                if(balls[i].explosion >12)
+                {
+                    sprite = 0;
+                }
+                else if (balls[i].explosion >8)
+                {
+                    sprite = 32;
+                }
+                else if (balls[i].explosion >4)
+                {
+                    sprite = 64;
+                }
+                else
+                {
+                    sprite = 96;
+                }
+                DrawTextureTiled(explosion,(Rectangle){sprite,0,32,32},(Rectangle){pos.x,pos.y,64,64}, (Vector2){0,0},0.0, 2.0, WHITE);
+
+                if(balls[i].explosion == 0)
+                {
+                    balls[i].enable = false;
+                }
+            }
+               
+
         }
 
         // Draw punto de mira
-        float mira_scale = (float)1/15; 
+        float mira_scale = (float)2/1; 
         Vector2 mira_pos = (Vector2){mousePos.x - mira_scale*(float)mira_image.width/2, mousePos.y - mira_scale*(float)mira_image.height/2};
         DrawTextureEx(mira_image, mira_pos, 0.0, mira_scale, LIGHTGRAY);  
 
@@ -290,14 +330,14 @@ int getdistance(Vector2 p1, Vector2 p2)
 void new_random_ball(void)
 {
     Vector2 newPos=(Vector2){GetRandomValue(screenWidth*0.2, screenWidth*0.8), GetRandomValue(screenHeight*0.2, screenHeight*0.8) };
-    Color newColor=(Color){GetRandomValue(0,255),GetRandomValue(0,255),GetRandomValue(0,255),255};
+    int newColor=GetRandomValue(1,3);
     int newRadious= GetRandomValue(70, 90);
     Vector2 newSpeed = (Vector2){0.01*GetRandomValue(10, 100),0.01*GetRandomValue(10, 100)};
 
     new_ball(newRadious, newSpeed, newPos, newColor);
 }
 
-void new_ball(int radius, Vector2 speed, Vector2 position, Color color)
+void new_ball(int radius, Vector2 speed, Vector2 position, int color)
 {
     if (num_balls < MAX_NUM_BALLS)
     {
@@ -305,6 +345,8 @@ void new_ball(int radius, Vector2 speed, Vector2 position, Color color)
         balls[num_balls].body = CreatePhysicsBodyCircle(position, balls[num_balls].radius, 10);
         balls[num_balls].body->restitution = 1;
         balls[num_balls].body->useGravity = true;
+        balls[num_balls].explosion = 0;
+        balls[num_balls].final_position =(Vector2){0.0, 0.0};
         balls[num_balls].enable = true;
         balls[num_balls].body->velocity = speed; 
         balls[num_balls].color=color;
